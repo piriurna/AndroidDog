3c4a61caf3ddd1de72313495f85c60030aba22af
Franco <francozalamena@gmail.com>
2022-09-28 16:04:35 +0100

    Breed

---
+++ domain/src/main/java/com/piriurna/domain/models/Breed.kt
@@ -0,0 +1,9 @@
+package com.piriurna.domain.models
+
+data class Breed(
+    val name : String,
+    val category : String,
+    val origin : String,
+    val temperament : String,
+    val imageUrl: String
+)
