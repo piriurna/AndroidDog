846ede2046dbd091cdb08c149f9ca9e2e1d50ae9
Franco <francozalamena@gmail.com>
2022-09-29 22:22:48 +0100

    Breed utils

--- domain/src/main/java/com/piriurna/domain/models/Breed.kt
+++ domain/src/main/java/com/piriurna/domain/models/Breed.kt
@@ -1,5 +1,7 @@
 package com.piriurna.domain.models
 
+import com.piriurna.domain.models.BreedImage.Companion.mockImages
+
 data class Breed(
     val id : Int,
     val name : String,
@@ -7,4 +9,38 @@ data class Breed(
     val origin : String,
     val temperament : String,
     val image: BreedImage
-)
+) {
+
+    fun getRelativeHeight(width : Int) : Int{
+        return image.getRelativeHeight(width)
+    }
+
+    companion object {
+        val mockBreeds = listOf(
+            Breed(
+                id = 0,
+                name = "Breed 1",
+                image = mockImages[0],
+                temperament = "temperament",
+                origin = "origin",
+                category = "category"
+            ),
+            Breed(
+                id = 1,
+                name = "Breed 2",
+                image = mockImages[1],
+                temperament = "temperament",
+                origin = "origin",
+                category = "category"
+            ),
+            Breed(
+                id = 2,
+                name = "Breed 3",
+                image = mockImages[2],
+                temperament = "temperament",
+                origin = "origin",
+                category = "category"
+            )
+        )
+    }
+}
